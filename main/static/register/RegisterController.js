32cbf46bea7a54fed3dec96a4d80b5d9aeed4b5e
stefanonu <onu.stefan19@yahoo.com>
2017-06-04 23:12:01 +0300

    Done: logare , inregistrare , integrare front-end-back-end , rutare pagini

--- main/static/register/RegisterController.js
+++ main/static/register/RegisterController.js
@@ -2,8 +2,35 @@
     'use strict';
     var app = angular.module('app', []);
 
-    app.controller('SignupController', ['$scope', signupController]);
-    function signupController($scope) {
+    app.controller('SignupController', ['$scope', '$http',signupController]);
+    function signupController($scope,$http) {
+
+        $scope.user = {
+            name:'',
+            email: '',
+            pwd: ''
+        };
+
+        function verifyUserData(){
+            if(_.isEmpty($scope.user.name) ||_.isEmpty($scope.user.pwd) || _.isEmpty($scope.user.email)){
+                return false;
+            } else return true;
+        }
+
+        $scope.Sign_Up=function () {
+            if(!verifyUserData()) {
+                return;
+            }
+            $http({
+                method: 'POST',
+                url: 'http://localhost:8000/api/users/create',
+                data: $scope.user,
+                headers:{"Access-Control-Allow-Origin":" *"}
+            }).then(function(res){
+                console.log(res)},
+                function(err){console.log(err)
+            });
+        };
 
         $scope.preferences = [
             {
